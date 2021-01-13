05acfa0f0adbc402e13c054315ec350f91229561
Sameer Marok <sameermarok@gmail.com>
2021-01-13 15:55:34 -0500

    New project

---
+++ blog_project/blog/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+from django.contrib.auth.models import User
+# Create your models here.
+
+class UserProfileInfo(models.Model):
+
+    user = models.OneToOneField(User,on_delete=models.DO_NOTHING)
+
+    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True) # we can keep published date empty so that it would just be a draft
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save() #saving model to database
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    def get_absolute_url(self): # after creating a post, this function will be called
+        return reverse("post_detail",kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post',related_name='comments',on_delete=models.CASCADE)
+    author = models.CharField(max_length=50)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list') # take user back to list os all posts
+
+    def __str__(self):
+        return self.text
